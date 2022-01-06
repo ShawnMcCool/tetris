68d4be2e036f6dc7cf0d4d718dd84a754f5f526b
Shawn McCool <shawn@mccool.email>
2022-01-06 09:30:30 +0100

    add test filtering

--- tests/run.php
+++ tests/run.php
@@ -2,9 +2,25 @@
 
 require 'test_bootstrap.php';
 
+/*
+ * get all test files in /tests/
+ */
 $searchDepth = '/,/*/,/*/*/,/*/*/*/,/*/*/*/*/,/*/*/*/*/*/,/*/*/*/*/*/*/,/*/*/*/*/*/*/*/,/*/*/*/*/*/*/*/*/,/*/*/*/*/*/*/*/*/*/';
 $testFiles = glob('tests{'.$searchDepth.'}*_tests.php', GLOB_BRACE);
 
-foreach ($testFiles as $testFile) {
+/*
+ * if command-line arguments are present, filter the files
+ */
+$searchQuery = implode(' ', array_slice($argv, 1, count($argv) - 1));
+
+$filesToRun = array_filter(
+    $testFiles,
+    fn($testFile) => str_contains($testFile, $searchQuery)
+);
+
+/*
+ * run the tests
+ */
+foreach ($filesToRun as $testFile) {
     require($testFile);
 }
